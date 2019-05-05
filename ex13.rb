da0f60c7d83c81e32a58c08edcc2ea5fcacbe804
Tuyen Dang <tuyendq@gmail.com>
2019-05-05 21:34:19 +0700

    add ex13.rb

---
+++ ex13.rb
@@ -0,0 +1,11 @@
+# Exercise 13: Parameters, Unpacking, Varibles
+first, second, third = ARGV
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
+
+# Study Drills section
+print "Give me string: "
+string = $stdin.gets.chomp
+puts string
