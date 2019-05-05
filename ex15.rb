ffee9f1637e084e1d2bcf16ba6299e2c4b2f1b82
Tuyen Dang <tuyendq@gmail.com>
2019-05-05 21:50:15 +0700

    add ex15.rb

---
+++ ex15.rb
@@ -0,0 +1,13 @@
+# Exercise 15: Reading Files
+filename = ARGV.first
+txt = open(filename)
+
+puts "Here's you file #{filename}:"
+print txt.read
+
+print "Type the filename again: "
+file_again = $stdin.gets.chomp
+
+txt_again = open(file_again)
+
+print txt_again.read
