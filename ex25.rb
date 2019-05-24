4b5840810a5b81d26e71ceee515afc23f86b2a65
Tuyen Dang <tuyendq@gmail.com>
2019-05-24 15:06:51 +0700

    add ex25.rb

---
+++ ex25.rb
@@ -0,0 +1,46 @@
+module Ex25
+
+    # This function will break up words for us
+    def Ex25.break_words(stuff)
+        words = stuff.split(' ')
+        return words
+    end
+
+    # Sorts the words
+    def Ex25.sort_words(words)
+        return words.sort
+    end
+
+    # Prints the first word after shifting it off.
+    def Ex25.print_first_word(words)
+        word = words.shift
+        puts word
+    end
+
+    # Print the last word after popping it off
+    def Ex25.print_last_word(words)
+        word = words.pop
+        put word
+    end
+
+    # Take in a full sentence and returns the sorted words
+    def Ex25.sort_sentence(sentence)
+        words = Ex25.break_words(sentence)
+        return Ex25.sort_words(words)
+    end
+
+    # Print the first and last words of the sentence
+    def Ex25.print_first_and_last(sentence)
+        words = Ex25.break_words(sentence)
+        Ex25.print_first_word(words)
+        Ex25.print_last_word(words)
+    end
+
+    # Sort the word then print the first and last one
+    def Ex25.print_first_and_last_sorted(sentence)
+        words = Ex25.sort_sentence(sentence)
+        Ex25.print_first_word(word)
+        Ex25.print_last_word(word)
+    end
+
+end
