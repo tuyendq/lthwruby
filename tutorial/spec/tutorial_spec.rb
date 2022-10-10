aab7ee212a2dd2d9d1842a0a42375f563cfc8ad0
Tuyen Dang <tuyendq@gmail.com>
2022-10-10 20:32:44 +0700

    using Watir with Ruby

--- tutorial/spec/tutorial_spec.rb
+++ tutorial/spec/tutorial_spec.rb
@@ -1,7 +1,18 @@
-require "watir"
+require "Watir"
 require "webdrivers"
 
 
 RSpec.describe Tutorial do
+    it "should explore w3schools.com" do
+        browser = Watir::Browser.new
+        browser.goto "https://www.w3schools.com/"
 
+        expect(browser.text_field(id: "search2").exists?).to equal(true)
+
+        browser.text_field(id: "search2").set "Ruby"
+
+        expect(browser.text_field(id: "search2").value).to match("Ruby")
+        browser.button(id: "learntocode_searchbtn").click
+
+    end
 end
